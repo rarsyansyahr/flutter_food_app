66a6ec5090b716a93856298d73bdc1f5868aa020
Arsyansyah <rarsyansyahr@gmail.com>
2024-07-02 13:37:22 +0700

    style: styling food list page

--- lib/features/foods/presentation/screens/food_list_screen/food_list_screen.dart
+++ lib/features/foods/presentation/screens/food_list_screen/food_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_food_app/features/foods/domain/entity/food_entity.dart';
 import 'package:flutter_food_app/features/foods/presentation/screens/bloc/food_list_bloc.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,6 +10,8 @@ class FoodListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return BlocConsumer<FoodListBloc, FoodListState>(
         listener: (context, state) {},
         builder: (context, state) {
@@ -23,35 +26,18 @@ class FoodListScreen extends StatelessWidget {
 
           if (state is FoodListGetFoodsSuccessState) {
             return Scaffold(
-              appBar: AppBar(
-                title: const Text("Foods Screen"),
-              ),
               body: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
                   itemCount: state.foods.length,
                   itemBuilder: (context, index) {
                     final food = state.foods[index];
 
-                    return ListTile(
-                      leading: CachedNetworkImage(
-                        imageUrl: food.thumbnail ?? "",
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                        placeholder: (context, url) => const Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                        errorWidget: (context, url, error) => const Center(
-                          child: Text(
-                            "No Image",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
+                    return Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: InkWell(
+                        onTap: () => context.goNamed("detail", extra: food.id),
+                        child: _listItem(textTheme, food),
                       ),
-                      title: Text("${food.name} - ${food.category}"),
-                      onTap: () => context.goNamed("detail", extra: food.id),
                     );
                   }),
             );
@@ -59,5 +45,77 @@ class FoodListScreen extends StatelessWidget {
 
           return const SizedBox();
         });
+  }
+
+  Widget _listItem(TextTheme textTheme, FoodEntity food) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(24),
+          child: Hero(
+            tag: food.thumbnail ?? "",
+            child: CachedNetworkImage(
+              imageUrl: food.thumbnail ?? "",
+              width: 320,
+              height: 320,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => const Center(
+                child: CircularProgressIndicator(),
+              ),
+              errorWidget: (context, url, error) => const Center(
+                child: Text(
+                  "No Image",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      food.name ?? "",
+                      style: textTheme.titleMedium,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      food.category ?? "",
+                      style: textTheme.bodySmall,
+                    )
+                  ],
+                ),
+              ),
+              Flexible(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Icon(Icons.flag_outlined),
+                      Text(
+                        food.area ?? '',
+                        style: textTheme.bodySmall,
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
